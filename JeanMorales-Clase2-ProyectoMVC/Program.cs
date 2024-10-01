b5b4a56b92c403a6f712c15b02b0a665c15212fe
JeanMV52 <JeanMorales-Clase2-MVC>
2024-10-01 07:47:33 -0500

    Cambio 1

--- JeanMorales-Clase2-ProyectoMVC/Program.cs
+++ JeanMorales-Clase2-ProyectoMVC/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 //hola
 
+//rngkrengfvnrvgrfmjv
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {

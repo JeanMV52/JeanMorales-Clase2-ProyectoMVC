c6929983ddc1a04b51740c89c561ac02ef5f60af
JeanMV52 <JeanMorales-Clase2-MVC>
2024-10-01 08:58:36 -0500

    Cambio 4

---
+++ JeanMorales-Clase2-ProyectoMVC/Models/Carrera.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JeanMorales_Clase2_ProyectoMVC.Models
+{
+    public class Carrera
+    {
+        [Required]
+        [Range(1,1000)]
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ JeanMorales-Clase2-ProyectoMVC/Models/EstudianteUDLA.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace JeanMorales_Clase2_ProyectoMVC.Models
+{
+    [Authorize] //Dar seguridad, afecta a todos los hijos de la clase
+    public class EstudianteUDLA
+    {
+        [Required]
+        public string IDBanner { get; set; }
+        [AllowNull]   //Aceptar nulos en correo
+        [EmailAddress] //Validar correo
+        public string Correo { get; set; }
+        [MaxLength(100)] //maximo 100 caracteres en nombre
+        
+        public string Nombre { get; set; }
+        public Carrera carrera { get; set; }
+    }
+}
